2410658d7ed330749b23aa93a5299d1c000ad952
liamor2 <87279441+liamor2@users.noreply.github.com>
2024-04-05 03:02:17 +0200

    Image generation is now working

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -12,21 +12,69 @@ const complexStoryRequest = async (req, res) => {
     try{
         // console.log(req)
         const storyParams = req.body;
+        if (storyParams.language === undefined) {
+            storyParams.language = "en";
+        }
         const language = lang[storyParams.language];
         // console.log(storyParams);
         // console.log(language);
-        const prompt = `${language.Header}${language.Length}${storyParams.longueur}${language.Age}${storyParams.ageCible}${language.Genre}${storyParams.themeHistoire}${language.MainCharacter}${storyParams.persoP.nom}, ${storyParams.persoP.age}, ${language.MainCharacterAge}${storyParams.persoP.personnalite}, ${storyParams.persoP.genre}.${language.Goal}${storyParams.defi}${language.Moral}${storyParams.morale}${language.Addition}${language.Items}${storyParams.elementsSupp.objets.join(", ")}${language.Encounters}${storyParams.elementsSupp.rencontres.join(", ")}`;
-        
-        console.log(prompt);
+        const prompt = 
+            language.Header+
+            language.Length+
+            storyParams.longueur+
+            language.Age+
+            storyParams.ageCible+ " " +
+            language.Genre+
+            storyParams.themeHistoire+
+            language.MainCharacter+
+            storyParams.persoP.nom+ ", " +
+            storyParams.persoP.age + " " +
+            language.CharacterAge+
+            storyParams.persoP.personnalite + ", "+
+            storyParams.persoP.genre +
+            language.SecondaryCharacter+
+            storyParams.persosSecondaires.map((perso) => {
+                return `${perso.nom}, ${language.SecondaryCharacterRole} ${perso.description}`;
+            }).join(",\n")+ ", "+
+            language.Goal+ 
+            storyParams.defi+ ", "+
+            language.Moral+
+            storyParams.morale+ ". "+
+            language.Addition+
+            language.Items+
+            storyParams.elementsSupp.objets.join(", ")+ ". "+
+            language.Encounters+
+            storyParams.elementsSupp.rencontres.join(", ") + ". ";
+        // console.log(prompt);
 
         const completion = await openai.chat.completions.create({
             messages: [{ role: "system", content: prompt }],
             model: "gpt-4-turbo-preview",
         });
-    
+        if (storyParams.image === true) {
+            const imagePrompt = language.ImagePrompt;
+            const image = await openai.images.generate(
+                {
+                    model: "dall-e-3",
+                    prompt: imagePrompt + completion.choices[0].message.content,
+                }
+            );
+            res.send(
+                {
+                    text: completion.choices[0].message.content,
+                    image: image.data[0].url
+                }
+            );
+            return;
+        }
         // console.log(completion.choices[0]);
-        res.send(completion.choices[0].message.content); 
-        res.send(prompt);
+        res.send(
+            {
+                text: completion.choices[0].message.content,
+                image: null
+            }
+        ); 
+        // res.send(prompt);
     }catch(error){
         console.log(error);
         res.status(500).send("An error occured");
@@ -34,12 +82,29 @@ const complexStoryRequest = async (req, res) => {
 };
 
 const simpleStoryRequest = async (req, res) => {
-    try{
+    try {
         // console.log(req)
         const storyParams = req.body;
+        if (storyParams.language === undefined) {
+            storyParams.language = "en";
+        }
         const language = lang[storyParams.language];
         // console.log(storyParams);
-            const prompt = `${language.Header}${language.Length}${storyParams.longueur}${language.Age}${storyParams.ageCible}${language.Genre}${storyParams.themeHistoire}${language.MainCharacter}${storyParams.persoP.nom}, ${storyParams.persoP.age}, ${language.MainCharacterAge}${storyParams.persoP.personnalite}, ${storyParams.persoP.genre}, ${language.SimpleStoryEnd}`
+            const prompt = 
+                language.Header+
+                language.Length+ 
+                storyParams.longueur+ 
+                language.Age+
+                storyParams.ageCible+ 
+                language.Genre+ 
+                storyParams.themeHistoire+ 
+                language.MainCharacter+ 
+                storyParams.persoP.nom+ ", "
+                storyParams.persoP.age+ ", "
+                language.MainCharacterAge+ 
+                storyParams.persoP.personnalite+ ", "
+                storyParams.persoP.genre+ ", "
+                language.SimpleStoryEnd + ". ";
         
         // console.log(prompt);
 
@@ -47,9 +112,29 @@ const simpleStoryRequest = async (req, res) => {
             messages: [{ role: "system", content: prompt }],
             model: "gpt-4-turbo-preview",
         });
+        if (storyParams.image === true) {
+            const imagePrompt = language.ImagePrompt;
+            const image = await openai.images.generate(
+                {
+                    model: "dall-e-3",
+                    prompt: imagePrompt + completion.choices[0].message.content,
+                }
+            );
+            res.send(
+                {
+                    text: completion.choices[0].message.content,
+                    image: image.data[0].url
+                }
+            );
+            return;
+        }
     
-        console.log(completion.choices[0]);
-        res.send(completion.choices[0].message.content); 
+        res.send(
+            {
+                text: completion.choices[0].message.content,
+                image: null
+            }
+        );
 
     }catch(error){
         console.log(error);
