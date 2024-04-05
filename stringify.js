af16618fa2b8dd6333248915ff024d21cd726182
SVergnol <samuel.vergnol@lucem-conseil.com>
2024-04-05 03:01:54 +0200

    swagger doc

---
+++ stringify.js
@@ -0,0 +1,18 @@
+const storyParams = {
+    ageCible: 8,
+
+    themeHistoire: "aventure fantastique",
+
+    persoP: {
+        nom: "Léo",
+        age: 8,
+        personnalite: "curieux",
+        genre: "M",
+    },
+    longueur: 250,
+    langue: "fr",
+    image: true
+}
+
+console.log(JSON.stringify(storyParams));
+
